4f02e4b07945ef1a7dbe07735772b44d8690dd2a
sushmita188 <sushmita@onetick.in>
2024-05-29 15:33:57 +0530

    add sitemap and robotstxt file and other heading changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta,Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 
@@ -7,7 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
-  title = 'zepcode';
+export class AppComponent implements OnInit{
+  // title = 'zepcode';
+  constructor(
+   
+    private title: Title,
+    private meta: Meta,
+  ) {}
+  ngOnInit(): void {
+    this.title.setTitle('MERN Full Stack Web Development Course - Zepcode')
+   
+      this.meta.updateTag({ name: 'title', content:`MERN Full Stack Web Development Course - Zepcode` });
+      this.meta.updateTag({ name: 'description', content: ` Zepcode offers you the best Mern Full stack web development course in Faridabad. If you want to become a full stack developer, you can contact us.` });
+    
+    
+  }
+
 }
